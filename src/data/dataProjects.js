36d2b4d84bc4e37555e14e283242fe2a7233fd61
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-09-16 17:29:04 -0300

    data icons and projects

---
+++ src/data/dataProjects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        id: "001",
+        name: undefined,
+        keywords: [""],
+        platform: ["Web"],
+        linguagens: [],
+        imagens:[],
+        video:[],
+        description: "",
+        url: undefined
+    },
+    {}
+]
+
+
